3a8a0e1e9ed4f144c6384e05c7cb2afe679b7b18
Heitor Tancredo <heitortancredo@gmail.com>
2023-12-22 17:28:48 -0300

    Add where by date when exists

--- repositories/stock_quotes.py
+++ repositories/stock_quotes.py
@@ -15,6 +15,11 @@ class StockQuotesRepository:
         return self.__get_stock_quotes_by_ticker(ticker, deal_date)
 
     def __get_stock_quotes_by_ticker(self, ticker: str | None, deal_date: str | None) -> dict:
+
+        if deal_date:
+            where_clause = text(f"""sq1.data_negocio >= '{deal_date}' and sq1.ticker = '{ticker}'""")
+        else:
+            where_clause = text(f"""sq1.ticker = '{ticker}'""")
         
         query = text(f"""
             with 
@@ -25,7 +30,7 @@ class StockQuotesRepository:
                     from 
                         stock_quotes sq1
                     where 
-                        sq1.data_negocio >= '2023-12-07' and sq1.ticker = '{ticker}' 
+                        {where_clause} 
                     group by 
                         sq1.data_negocio, sq1.ticker 
                     order by 
@@ -59,21 +64,3 @@ class StockQuotesRepository:
         result = self.__database.execute(query)
 
         return result.mappings().all()
-        
-    #     max_range_value = (self.__database.query(
-    #         StockQuotes.id,
-    #         StockQuotes.preco_negocio
-    #     ).filter(StockQuotes.ticker == ticker)
-    #                        .order_by(StockQuotes.preco_negocio.desc()).limit(1).subquery())
-
-    #     result = self.__database.query(
-    #         StockQuotes
-    #     ).filter(StockQuotes.id == max_range_value.c.id)
-
-    #     return jsonable_encoder(result.all())
-
-    def get_highest_number_of_deals(self, start_date: str | None) -> dict:
-        if start_date:
-            pass
-
-        all_stock = self.__database.query(StockQuotes)
