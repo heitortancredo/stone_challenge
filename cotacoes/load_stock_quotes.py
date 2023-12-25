4a359c8dd7fb16ae232608c0fecba3f49ab47c96
Heitor Tancredo <heitortancredo@gmail.com>
2023-12-25 18:55:24 -0300

    Imporve load_data script

--- cotacoes/load_stock_quotes.py
+++ cotacoes/load_stock_quotes.py
@@ -1,8 +1,8 @@
-from sqlalchemy.types import Integer, BigInteger, DateTime, String, Float
-from sqlalchemy.engine.url import URL
-from sqlalchemy import create_engine
 import pandas as pd
-
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import Session
+from sqlalchemy.types import DateTime, Float, Integer, String
 
 STOCK_QUOTES_FILES = [
     # "sample.csv"
@@ -31,30 +31,34 @@ DATABASE_URL = URL.create(
 def main():
     psql_engine = create_engine(DATABASE_URL, pool_recycle=3600, pool_pre_ping=True)
 
-    for file_csv in STOCK_QUOTES_FILES:
-        print(f"importing {file_csv}...")
+    with Session(psql_engine) as session:
+        for file_csv in STOCK_QUOTES_FILES:
+            print(f"importing {file_csv}...")
 
-        df_csv = pd.read_csv(file_csv, delimiter=";", decimal=",", skip_blank_lines=True)
+            df_csv = pd.read_csv(file_csv, delimiter=";", decimal=",", skip_blank_lines=True)
 
-        df_filtered = pd.DataFrame(
-            df_csv, columns=["CodigoInstrumento", "HoraFechamento", "DataNegocio", "PrecoNegocio", "QuantidadeNegociada"]
-        )
-        df_filtered.columns = ["ticker", "hora_fechamento", "data_negocio", "preco_negocio", "quantidade_negociada"]
-        df_filtered.to_sql(
-            "stock_quotes",
-            psql_engine,
-            if_exists="append",
-            index=True,
-            index_label="id",
-            dtype={
-                "id": BigInteger,
-                "ticker": String(length=128),
-                "hora_fechamento": String(length=128),
-                "preco_negocio": Float,
-                "quantidade_negociada": Integer,
-                "data_negocio": DateTime
-            }
-        )
+            df_filtered = pd.DataFrame(
+                df_csv,
+                columns=["CodigoInstrumento", "HoraFechamento", "DataNegocio", "PrecoNegocio", "QuantidadeNegociada"],
+            )
+            df_filtered.columns = ["ticker", "hora_fechamento", "data_negocio", "preco_negocio", "quantidade_negociada"]
+            df_filtered.to_sql(
+                "stock_quotes",
+                psql_engine,
+                if_exists="append",
+                index=False,
+                index_label="id",
+                chunksize=1000,
+                method="multi",
+                dtype={
+                    "ticker": String(length=128),
+                    "hora_fechamento": String(length=128),
+                    "preco_negocio": Float,
+                    "quantidade_negociada": Integer,
+                    "data_negocio": DateTime,
+                },
+            )
+            session.commit()
 
 
 if __name__ == "__main__":
