aae4f43cb71d65f905f309ea15641d6754c556d6
Heitor Tancredo <heitortancredo@gmail.com>
2023-12-26 10:15:01 -0300

    Refactoring of load_data script

--- cotacoes/load_stock_quotes.py
+++ cotacoes/load_stock_quotes.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import Session
 from sqlalchemy.types import DateTime, Float, Integer, String
 
-from configs import DATABASE_HOSTNAME, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_USERNAME
+from configs import DATABASE_HOSTNAME, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_TABLE, DATABASE_USERNAME
 
 STOCK_QUOTES_FILES = [
     # "sample.csv"
@@ -26,36 +27,42 @@ DATABASE_URL = URL.create(
 )
 
 
+def read_data_from_csv(csv_file: str) -> pd.DataFrame:
+    return pd.read_csv(csv_file, delimiter=";", decimal=",", skip_blank_lines=True)
+
+
+def load_to_database(input_data: pd.DataFrame, datatbase_engine: sqlalchemy.engine.base.Engine) -> None:
+    df_filtered = pd.DataFrame(
+        input_data,
+        columns=["CodigoInstrumento", "HoraFechamento", "DataNegocio", "PrecoNegocio", "QuantidadeNegociada"],
+    )
+    df_filtered.columns = ["ticker", "hora_fechamento", "data_negocio", "preco_negocio", "quantidade_negociada"]
+    df_filtered.to_sql(
+        DATABASE_TABLE,
+        datatbase_engine,
+        if_exists="append",
+        index=False,
+        index_label="id",
+        chunksize=1000,
+        method="multi",
+        dtype={
+            "ticker": String(length=128),
+            "hora_fechamento": String(length=128),
+            "preco_negocio": Float,
+            "quantidade_negociada": Integer,
+            "data_negocio": DateTime,
+        },
+    )
+
+
 def main():
     psql_engine = create_engine(DATABASE_URL, pool_recycle=3600, pool_pre_ping=True)
 
     with Session(psql_engine) as session:
         for file_csv in STOCK_QUOTES_FILES:
             print(f"importing {file_csv}...")
-
-            df_csv = pd.read_csv(file_csv, delimiter=";", decimal=",", skip_blank_lines=True)
-
-            df_filtered = pd.DataFrame(
-                df_csv,
-                columns=["CodigoInstrumento", "HoraFechamento", "DataNegocio", "PrecoNegocio", "QuantidadeNegociada"],
-            )
-            df_filtered.columns = ["ticker", "hora_fechamento", "data_negocio", "preco_negocio", "quantidade_negociada"]
-            df_filtered.to_sql(
-                "stock_quotes",
-                psql_engine,
-                if_exists="append",
-                index=False,
-                index_label="id",
-                chunksize=1000,
-                method="multi",
-                dtype={
-                    "ticker": String(length=128),
-                    "hora_fechamento": String(length=128),
-                    "preco_negocio": Float,
-                    "quantidade_negociada": Integer,
-                    "data_negocio": DateTime,
-                },
-            )
+            input_df = read_data_from_csv(file_csv)
+            load_to_database(input_df, datatbase_engine=psql_engine)
             session.commit()
 
 
